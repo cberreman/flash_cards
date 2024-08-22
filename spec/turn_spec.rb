98266ce05751ce43fecd3da1e4a9213098e3572f
Cole Berreman <c.berrem@gmail.com>
2024-08-22 16:37:07 -0600

    initial commit

---
+++ spec/turn_spec.rb
@@ -0,0 +1,28 @@
+require './lib/turn'
+require './lib/card'
+
+RSpec.describe Turn do
+it 'exists' do 
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)    
+    turn = Turn.new("Juneau", card)
+    
+    expect(turn).to be_instance_of(Turn)
+end
+
+it 'has a guess' do 
+    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)        
+    turn = Turn.new("Juneau", card)
+    #  require 'pry' ; binding.pry   
+    expect(turn.guess).to eq('Juneau')
+end
+
+
+
+
+
+
+
+
+
+end
+    
